9dcc5cfcb02e9715b7fcd74fd8d037f33791a852
acui51 <acui@stanford.edu>
2022-02-25 02:52:48 -0800

    .

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import PagerView from "react-native-pager-view";
+import { useState, useEffect } from "react";
+
+const LOAD_TIME = 100;
+const INTERVAL = 25;
 
 export default function App() {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prevProgress) => {
+        if (prevProgress + 10 >= LOAD_TIME) {
+          clearInterval(interval);
+        }
+        return prevProgress + 10;
+      });
+    }, INTERVAL);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PagerView style={styles.pagerView} initialPage={0} orientation="vertical">
+      <View key="1" style={styles.container}>
+        <Text>{progress}</Text>
+        <View
+          style={{
+            position: "relative",
+            width: 500,
+            height: 16,
+            backgroundColor: "white"
+          }}
+        >
+          <View
+            style={{
+              position: "absolute",
+              width: 500 * (progress / LOAD_TIME),
+              height: 16,
+              backgroundColor: "green"
+            }}
+          />
+        </View>
+      </View>
+      <View key="2" style={styles.container}>
+        <Text>Second page</Text>
+      </View>
+    </PagerView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  pagerView: {
+    flex: 1
+  }
 });
